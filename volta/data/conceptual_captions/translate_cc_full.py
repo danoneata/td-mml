1e35e4bc0c655a8dc2c72115c93136e2d5ceb4bc
Dan Oneata <dan.oneata@gmail.com>
2022-04-28 19:17:06 +0300

    ENH Translate only given keys

--- volta/data/conceptual_captions/translate_cc_full.py
+++ volta/data/conceptual_captions/translate_cc_full.py
@@ -41,9 +41,10 @@ TARGET_LANGUAGES = read("data/langs-iglue.txt")
 @click.option("-l", "--language", type=click.Choice(TARGET_LANGUAGES), help="target language")
 @click.option("-m", "--model", "model_type", type=click.Choice(MODEL_TYPES), help="translation model")
 @click.option("-b", "--batch-size", "batch_size", type=click.INT, default=None, help="if not set, estimated based on the machine")
+@click.option("-k", "--path-keys", "path_keys", help="json mapping a language to the keys that we need to translate")
 @click.option("--device")
 @click.option("-v", "--verbose", is_flag=True)
-def main(split, language, model_type, batch_size=None, device="cuda", verbose=False):
+def main(split, language, model_type, batch_size=None, path_keys=None, device="cuda", verbose=False):
     # load English data and cached translations from `folder_input`
     folder_input = model_type + "-seed-1337"
     folder_output = model_type + "-full"
@@ -57,9 +58,16 @@ def main(split, language, model_type, batch_size=None, device="cuda", verbose=Fa
     model = load_model(model_type, device)
     batch_size = batch_size or get_batch_size(model_type, device)
 
+    if path_keys:
+        with open(path_keys, "r") as f:
+            lang_to_keys = json.load(f)
+            keys = lang_to_keys[languages]
+    else:
+        keys = data.keys()
+
     path_cache = os.path.join(PATH_DATA, folder_input, ".cache", f"{language}-{split}")
     with shelve.open(path_cache) as data_cached:
-        data_tr = translate(data, model, language, batch_size, data_cached, verbose=verbose)
+        data_tr = translate(data, model, language, batch_size, data_cached, keys=keys, verbose=verbose)
         # data_tr = {k: v for k, v in tqdm.tqdm(data_cached.items())}
         save_data(data_tr, split, language, folder_output)
 
