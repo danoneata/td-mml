4c8a2ac2e5b3641d9a734df1046aa0f5f08ddf63
Dan Oneata <dan.oneata@gmail.com>
2022-05-18 18:09:05 +0300

    WIP Refactor translation features

--- volta/data/conceptual_captions/compute_translation_feature.py
+++ volta/data/conceptual_captions/compute_translation_feature.py
@@ -1,20 +1,30 @@
 import json
 import sys
 import pdb
+import pickle
 import os
 
+from typing import Any, List, Dict, TypedDict
+
+import click
+
 from tqdm import tqdm
+from toolz import merge
+
+from translate_cc_full import load_data
 
 
-def load_data(lang, version):
-    with open(f"data/cc/analysis-cache-v{version:03d}/{lang}.json", "r") as f:
-        return json.load(f)
+# def load_data(lang, version):
+#     with open(f"data/cc/analysis-cache-v{version:03d}/{lang}.json", "r") as f:
+#         return json.load(f)
 
 
-def save_data(data, lang, version):
-    new_version = version + 1
-    os.makedirs(f"data/cc/analysis-cache-v{new_version:03d}", exist_ok=True)
-    with open(f"data/cc/analysis-cache-v{new_version:03d}/{lang}.json", "w") as f:
+BASE_PATH = "data/cc/analysis-cache"
+Scored = TypedDict("Scored", {"key": str, "score": float})
+
+
+def save_data(data: Dict, lang: str) -> None:
+    with open(f"data/cc/analysis-cache/{lang}.json", "w") as f:
         json.dump(data, f, indent=4)
 
 
@@ -30,28 +40,119 @@ def compute_lm_score(data):
     pdb.set_trace()
 
 
-def compute_blue_translated(data):
+def compute_uniformity(data: Dict) -> List[Scored]:
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
+
+    def do1(datum):
+        tokens = tokenizer(datum["text-tgt"])["input_ids"]
+        unique = set(tokens)  # unique tokens
+        return 1 - len(unique) / len(tokens)
+
+    return [
+        {
+            "key": datum["key"],
+            "score": do1(datum),
+        }
+        for datum in tqdm(data)
+    ]
+
+
+def compute_len_ratio(data: Dict) -> List[Scored]:
+    pass
+
+
+def compute_sim_tgt_src(data: Dict) -> List[Scored]:
+    pass
+
+
+def compute_bleu_translated(data: Dict) -> List[Scored]:
     from nltk.translate.bleu_score import sentence_bleu
 
-    for datum in tqdm(data):
-        datum["scores"]["sim-tgt-src-bleu"] = sentence_bleu(
-            [datum["text-src"]], datum["text-tgt"]
-        )
+    def do1(datum):
+        return sentence_bleu([datum["text-src"]], datum["text-tgt"])
 
-    return data
+    return [
+        {
+            "key": datum["key"],
+            "score": do1(datum),
+        }
+        for datum in tqdm(data)
+    ]
 
 
-def main():
-    lang = sys.argv[1]
-    version = int(sys.argv[2])
+def cache(func, name):
+    path = os.path.join(BASE_PATH, ".cache", name + ".pkl")
+    def wrapped(*args, **kwargs):
+        if os.path.exists(path):
+            with open(path, "rb") as f:
+                return pickle.load(f)
+        else:
+            res = func(*args, **kwargs)
+            with open(path, "wb") as f:
+                pickle.dump(res, f)
+            return res
 
-    assert lang in "id sw ta tr zh".split()
+    return wrapped
 
-    data = load_data(lang, version)
-    # data_updated = compute_lm_score(data)
-    data_updated = compute_blue_translated(data)
 
-    save_data(data_updated, lang, version)
+def merge_features(feature_data, data):
+    features = feature_data.keys()
+    return [
+        merge(data[i], {"scores": {f: feature_data[f][i]["score"] for f in features}})
+        for i in range(len(data))
+    ]
+
+
+FEATURES = {
+    "uniformity": compute_uniformity,
+    "len-ratio": compute_len_ratio,
+    "sim-tgt-src": compute_sim_tgt_src,
+    "sim-tgt-src-bleu": compute_bleu_translated,
+}
+
+
+
+LANGUAGES = "ar bg bn da de el fr id ja ko pt ru sw ta tr vi zh".split()
+
+
+@click.command(help="Extract translation features")
+@click.option(
+    "-l",
+    "--lang",
+    type=click.Choice(LANGUAGES),
+    help="which language to analyze",
+)
+@click.option(
+    "-f",
+    "--features",
+    type=click.Choice(FEATURES),
+    multiple=True,
+    help="name of features",
+)
+def main(lang: str, features: List[str]) -> None:
+
+    split = "train"
+    folder = "m2m-100-lg-full"
+
+    data_src = load_data(split, "en", folder)
+    data_tgt = load_data(split, lang, folder)
+
+    with open(os.path.join(BASE_PATH, "keys.json"), "r") as f:
+        keys = json.load(f)
+
+    data = [
+        {
+            "key": key,
+            "text-src": data_src[key],
+            "text-tgt": data_tgt[key],
+        }
+        for key in tqdm(keys)
+    ]
+
+    feature_data = {feat: cache(FEATURES[feat], lang + "-" + feat)(data) for feat in features}
+    feature_data = merge_features(feature_data, data)
+    save_data(feature_data, lang)
 
 
 if __name__ == "__main__":
